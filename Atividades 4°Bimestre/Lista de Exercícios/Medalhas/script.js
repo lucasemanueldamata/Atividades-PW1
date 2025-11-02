b192b77726a40c06b047e79a9d01f23b64da54db
lucasemanueldamata <lucas.emanueldamata@gmail.com>
2025-11-02 13:32:15 -0300

    add

--- Atividades 4°Bimestre/Lista de Exercícios/Medalhas/script.js
+++ Atividades 4°Bimestre/Lista de Exercícios/Medalhas/script.js
@@ -2,6 +2,12 @@ function verificarMedalha() {
     var pontuacao = parseInt(document.getElementById("pontuacao").value);
     var medalha = "";
 
+    if (isNaN(pontuacao) || pontuacao < 0 || pontuacao > 100) {
+        document.getElementById("resultado").innerText = "Por favor insira uma pontuação válida entre 0 e 100.";
+        document.getElementById("resultado").className = "error";
+        return;
+    }
+
     if (pontuacao >= 90) {
         medalha = "Ouro";
     } else if (pontuacao >= 75) {
@@ -14,7 +20,7 @@ function verificarMedalha() {
 
     document.getElementById("resultado").innerText = "Medalha: " + medalha;
 }
-function limparCampo() {
+function resetar () {
     document.getElementById("pontuacao").value = "";
     document.getElementById("resultado").innerText = "";
     document.getElementById("resultado").className = "";
